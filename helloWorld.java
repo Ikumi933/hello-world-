6f29f06560d077d7b22dfb38b000590869c47127
Ikumi333 <uemuraikumi0531@gmail.com>
2025-03-25 13:33:08 -0400

    Revert "Add another statement"
    This reverts commit 26649264a8284ff39c8f237c2967d0eaf27bad32.

--- helloWorld.java
+++ helloWorld.java
@@ -2,7 +2,6 @@ public class helloWorld{
 
 public static void main(String[] args)
 {
-   System.out.println("Hellow world!!");
-   System.out.println("Program modified, but able to revert to original version using GitHub Desktop");
+   System.out.print("Hellow world!!");
 }
 }
